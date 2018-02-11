7dbe8316e41450d28ae7e231283dcad9c6ef7308
Abdelrahman Yousry <34912686+AYoussry@users.noreply.github.com>
2018-02-11 18:10:12 +0200

    Updating models.py and adding users, category

--- goodreads/books/models.py
+++ goodreads/books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 # class Author (models.Model):
 #     author_name = models.CharField(max_length = 100)
@@ -8,24 +9,54 @@ from django.db import models
 
 #     def __str__ (self):
 #         return self.author_name + '-' + self.author_bio 
-class Author (models.Model):
-    author_name = models.CharField(max_length = 50)
-    author_bio = models.CharField(max_length = 1000)
-    author_pp = models.CharField(max_length = 1000)
+class Author(models.Model):
+    author_name = models.CharField(max_length=50)
+    author_bio = models.CharField(max_length=1000)
+    author_pp = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.author_name + '-' + self.author_bio 
+        return self.author_name
 
-class Book (models.Model):
-    book_title = models.CharField(max_length = 50)
-    book_category = models.CharField(max_length = 50)
-    book_description = models.CharField(max_length =1000)
+
+class Book(models.Model):
+    book_title = models.CharField(max_length=50)
+    book_category = models.CharField(max_length=50)
+    book_description = models.CharField(max_length=1000)
     book_cover = models.CharField(max_length=1000)
-    #book = models.ForeignKey(book, on_delete=models.CASCADE)
-    #book = models.ForeignKey(Author, on_delete=models.CASCADE)
+    Author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.book_title + '-' + self.book_category + '-' + self.book_description 
+        return self.book_title
 
 
-    
+class Category(models.Model):
+    cat_genre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.cat_genre
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=15)
+    user_password = models.CharField(max_length=20)
+    user_email = models.CharField(max_length=50)
+    user_pp = models.CharField(max_length=1000)
+
+    read = models.ManyToManyField(Book, related_name='user_book_read')
+    wishlist = models.ManyToManyField(Book, related_name='user_book_wishlist')
+    user_book_rate = models.ManyToManyField(Book, through='Rate')
+    user_category_fav = models.ManyToManyField(Category)
+    user_author_follow = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.user_name + '-' + self.user_email
+
+
+class Rate(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE)  # when deleting the user you have to delete his rate to get rid of fake accounts rating
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rate = models.IntegerField()
+
+    def __str__(self):
+        return self.rate
