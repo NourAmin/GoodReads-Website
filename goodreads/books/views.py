cabe9d8f570ffc60de93d2bc6de14f6500924be7
NourAmin <34912829+NourAmin@users.noreply.github.com>
2018-02-11 23:56:58 +0200

    Update views.py

--- goodreads/books/views.py
+++ goodreads/books/views.py
@@ -1,24 +1,20 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
-#from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 from .models import Book
 from .models import Author
+
 
 # Create your views here.
  
 def index(request):
-    return HttpResponse("<h1> goodreads home </h1>")
-    all_books =Book.objects.all()
-    #template = loader_get_template('books/index.html')
-    #context ={'all_books': all_books}
-    #return HttpResponse (template.render(context,request))
-    # html =  ''
-    # for book in all_books :
-    #         url = '/books' + str(Book_id) + '/'
-    #         #html += "<a href=" + url + "">"" + Book.book_title + "</a>"
-    # return HttpResponse(url)
-
+    book_list =Book.objects.all()
+    context = {'book_list': book_list}
+    return render(request, 'books/book_list.html', context)
+   
 def detail(request, Book_id):
+    try:
     #tit = Book.book_title
-    return HttpResponse("this is details for book id:" + str(Book_id))
-#"<h2>this is details for book title:</h2>" + str(book_title) +"<h2> it is category is </h2>" + str(book_category) +"<h2> it discuss </h2>" + str(book_description) +"</h2>")
+        book = Book.objects.get(pk=Book_id)
+    except Book.DoesNotExist:
+        raise Http404("book does not exist")    
+    return render(request, 'books/detail.html' , {'book' :book})
