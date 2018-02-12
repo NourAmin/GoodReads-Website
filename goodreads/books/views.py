4807940902fb639baacc7cdf0edb24d84f36a3de
AYoussery <byoussery1993@gmail.com>
2018-02-12 08:39:50 +0200

    added user models and updating old models of author and books

--- goodreads/books/views.py
+++ goodreads/books/views.py
@@ -1,5 +1,6 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 #from django.template import loader
 from .models import Book
 from .models import Author
@@ -20,5 +21,5 @@ def index(request):
 
 def detail(request, Book_id):
     #tit = Book.book_title
-    return HttpResponse("this is details for book id:" + str(Book_id))
+    return HttpResponse("this is details for book id:" + str(Book_id))  `
 #"<h2>this is details for book title:</h2>" + str(book_title) +"<h2> it is category is </h2>" + str(book_category) +"<h2> it discuss </h2>" + str(book_description) +"</h2>")
