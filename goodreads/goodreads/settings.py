48648c7493f01a51782fcdef9d476800e51ba384
islamhathout <i.hathout94@gmail.com>
2018-02-13 05:27:34 +0200

    Adding authors and users

--- goodreads/goodreads/settings.py
+++ goodreads/goodreads/settings.py
@@ -76,11 +76,8 @@ WSGI_APPLICATION = 'goodreads.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'books',
-        'USER': 'team',
-        'PASSWORD': 'django',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
