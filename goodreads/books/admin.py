235c8849e9e5198617f796b48922e8a13b0a6bfe
NourAmin <34912829+NourAmin@users.noreply.github.com>
2018-02-14 18:20:51 +0200

    Update admin.py

--- goodreads/books/admin.py
+++ goodreads/books/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
-# Register your models here.
+from .models import Author
+from .models import Category
+# # Register your models here.
 admin.site.register(Book)
-
+admin.site.register(Category)
