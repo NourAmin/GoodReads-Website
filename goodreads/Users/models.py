944ea6cfdf1637e084f72b2569609748f5996bd0
Abdelrahman Yousry <34912686+AYoussry@users.noreply.github.com>
2018-02-14 17:30:14 +0200

    Update models.py

--- goodreads/Users/models.py
+++ goodreads/Users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from Authors.models import Author
-
+from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
@@ -9,10 +9,24 @@ class UserProfile(models.Model):
 
     read = models.ManyToManyField('books.Book', related_name='user_book_read')
     wishlist = models.ManyToManyField('books.Book', related_name='user_book_wishlist')
-    #user_book_rate = models.ManyToManyField('books.Book', through='Rate')
+    user_book_rate = models.ManyToManyField('books.Book', through='BookRate')
     user_category_fav = models.ManyToManyField('books.Category')
     user_author_follow = models.ManyToManyField(Author)
 
     def __str__(self):
-        return self.user_name + '-' + self.user_email
+        return self.user.username
 
+    def create_profile(sender, **kwargs):
+        if kwargs['created']:
+            user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+    post_save.connect(create_profile, sender=User)
+
+class BookRate(models.Model):
+    user = models.ForeignKey(UserProfile,
+                             on_delete=models.CASCADE)  # when deleting the user you have to delete his rate to get rid of fake accounts rating
+    book = models.ForeignKey('books.Book', on_delete=models.CASCADE)
+    rate = models.IntegerField()
+
+    def __str__(self):
+        return self.rate
