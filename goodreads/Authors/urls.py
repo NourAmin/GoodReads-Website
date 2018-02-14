f68012cf7faeee05d8770c60acca4b795d39bf62
Abdelrahman Yousry <34912686+AYoussry@users.noreply.github.com>
2018-02-14 18:36:41 +0200

    Update urls.py

--- goodreads/Authors/urls.py
+++ goodreads/Authors/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, re_path
 from django.conf.urls import url
 from . import views
-
-
 urlpatterns = [
-    
+    re_path (r'^$', views.index, name ='AuthorListView'),
+    re_path (r'^(?P<Author_id>[0-9]+)/$', views.detail, name = 'detail'),
+    #url(r'^$',views.index) 
+
+    #re_path (r'^(?P<Book_id>[0-9]+)/favorite/$', views.favorite, name = 'favorite'),
 ]
+
+
