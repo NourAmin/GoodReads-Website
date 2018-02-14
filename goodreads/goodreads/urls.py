dc813dce548465c7a5010137e910122886e52f33
NourAmin <34912829+NourAmin@users.noreply.github.com>
2018-02-14 18:27:59 +0200

    Update urls.py

--- goodreads/goodreads/urls.py
+++ goodreads/goodreads/urls.py
@@ -1,6 +1,9 @@
 from django.urls import include, path, re_path
 from django.contrib import admin
 from goodreads import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.login_redirect, name='login_redirect'),
     path('admin/', admin.site.urls),
@@ -8,4 +11,4 @@ urlpatterns = [
     path('users/', include('Users.urls')),
     path('authors/',include('Authors.urls')),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
