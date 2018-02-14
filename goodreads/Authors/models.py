c3ba64aa779c5038ff9b83e3a81c75c41b6471f2
Abdelrahman Yousry <34912686+AYoussry@users.noreply.github.com>
2018-02-14 18:31:11 +0200

    Update models.py

--- goodreads/Authors/models.py
+++ goodreads/Authors/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Author(models.Model):
     author_name = models.CharField(max_length=50)
     author_bio = models.CharField(max_length=1000)
-    author_pp = models.CharField(max_length=1000)
+    author_pp = models.ImageField(blank = 'true',upload_to='profile_image')
 
     def __str__(self):
         return self.author_name
