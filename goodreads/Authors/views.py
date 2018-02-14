6bba9a5afe4626383f55c2c1ac63bcc3c8848926
Abdelrahman Yousry <34912686+AYoussry@users.noreply.github.com>
2018-02-14 18:39:59 +0200

    Update views.py

--- goodreads/Authors/views.py
+++ goodreads/Authors/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render ,get_object_or_404
+from .models import *
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the authors page")
 # Create your views here.
+
+def detail(request, Author_id):
+    try:
+
+         author= Author.objects.get(pk=Author_id)
+    except Author.DoesNotExist:
+        raise Http404("book does not exist")
+    return render(request, 'authors/detail.html' , {'author' :author})
+
